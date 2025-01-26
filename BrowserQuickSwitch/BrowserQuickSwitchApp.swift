25165c6802ad2c3d7fd83a5e2f372d7c46715c9f
Gitnapp <41632082+Gitnapp@users.noreply.github.com>
2025-01-26 20:03:19 +0800

    Initial Commit

---
+++ BrowserQuickSwitch/BrowserQuickSwitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrowserQuickSwitchApp.swift
+//  BrowserQuickSwitch
+//
+//  Created by 余子健 on 1/26/25.
+//
+
+import SwiftUI
+
+@main
+struct BrowserQuickSwitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
